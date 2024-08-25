0df7de2c8fda459c58373dd582688c66ddbc1438
a-gataullin <gataullin12@mail.ru>
2024-08-25 12:59:12 +0300

    wip2

--- sharding-repl-cache/scripts/mongo-init.sh
+++ sharding-repl-cache/scripts/mongo-init.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-###
-# Инициализируем бд
-###
-
-docker exec -it configSrv mongosh --port 27017
+docker compose exec -T configSrv mongosh --port 27017 <<EOF
 rs.initiate(
   {
     _id : "config_server",
@@ -14,25 +10,25 @@ rs.initiate(
     ]
   }
 );
-exit();
+EOF
 
-docker exec -it shard1_1 mongosh --port 27011
+docker compose exec -T shard1_1 mongosh --port 27011 <<EOF
 rs.initiate({_id: "shard1", members: [
     {_id: 0, host: "shard1_1:27011"},
     {_id: 1, host: "shard1_2:27012"},
     {_id: 2, host: "shard1_3:27013"}
-]}) 
-exit();
+]});
+EOF
 
-docker exec -it shard2_1 mongosh --port 27021
+docker compose exec -T shard2_1 mongosh --port 27021 <<EOF
 rs.initiate({_id: "shard2", members: [
     {_id: 0, host: "shard2_1:27021"},
     {_id: 1, host: "shard2_2:27022"},
     {_id: 2, host: "shard2_3:27023"}
-]}) 
-exit();
+]});
+EOF
 
-docker exec -it mongos_router mongosh --port 27020
+docker compose exec -T mongos_router mongosh --port 27020 <<EOF
 sh.addShard( "shard1/shard1_1:27011");
 sh.addShard( "shard2/shard2_1:27021");
 
@@ -41,27 +37,18 @@ sh.shardCollection("somedb.helloDoc", { "name" : "hashed" } )
 
 use somedb
 
-for(var i = 0; i < 1000; i++) db.helloDoc.insert({age:i, name:"ly"+i});
+for(var i = 0; i < 1000; i++) db.helloDoc.insertOne({age:i, name:"ly"+i});
+EOF
 
-db.helloDoc.countDocuments();
+# docker exec -it shard1_1 mongosh --port 27011
+# db.helloDoc.countDocuments();
+# exit();
 
-exit(); 
-
-###
-# Инициализируем бд
-###
-
-# docker compose exec -T mongodb1 mongosh <<EOF
-# use somedb
-# for(var i = 0; i < 1000; i++) db.helloDoc.insertOne({age:i, name:"ly"+i})
-# EOF
-
-docker exec -it shard1_1 mongosh --port 27011
-db.helloDoc.countDocuments();
-exit();
-
-docker exec -it shard2_1 mongosh --port 27021
-db.helloDoc.countDocuments();
-exit();
+# docker exec -it shard2_1 mongosh --port 27021
+# db.helloDoc.countDocuments();
+# exit();
 
 
+docker compose exec -T redis_1 sh <<EOF
+echo "yes" | redis-cli --cluster create   173.17.0.2:6379   173.17.0.3:6379   173.17.0.4:6379   173.17.0.5:6379   173.17.0.6:6379   173.17.0.7:6379   --cluster-replicas 1
+EOF
